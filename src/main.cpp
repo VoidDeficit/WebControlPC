f3cb10f735199f33272eaf0b4c68f3699b0b4ad6
Krotenhannes <Tom_b@fietje.de>
2023-02-17 21:18:41 +0100

    move secrets.h to include

--- src/main.cpp
+++ src/main.cpp
@@ -61,12 +61,12 @@ Task checkPCStateTask(1000, TASK_FOREVER, &checkPCState);
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
     case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
+        //Serial.printf("[%u] Disconnected!\n", num);
       break;
     case WStype_CONNECTED: {
-        IPAddress ip = webSocket.remoteIP(num);
+        //IPAddress ip = webSocket.remoteIP(num);
         webSocket.broadcastTXT(event);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
       }
       break;
     case WStype_TEXT:
